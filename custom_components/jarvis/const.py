f60c56e120da5d5af0fe204a80098e90c1b43229
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-08-28 10:06:15 +0200

    V3: port Core V2 constants

---
+++ custom_components/jarvis/const.py
@@ -0,0 +1,17 @@
+"""Constants for JARVIS Core 3."""
+DOMAIN = "jarvis"
+PANEL_URL = "jarvis"
+FRONTEND_URL = "/jarvis_core"
+FRONTEND_FILE = f"{FRONTEND_URL}/jarvis-core.js"
+PREFERRED_ASSIST_FILE = f"{FRONTEND_URL}/jarvis-preferred-assist.js"
+ENERGY_ENTITIES = {
+    "production": "sensor.envoy_122323101280_production_solaire_instantanee",
+    "consumption": "sensor.envoy_122323101280_consommation_electrique_actuelle",
+    "import": "sensor.puissance_import_reseau",
+    "export": "sensor.puissance_export_reseau",
+}
+SATELLITE_ENTITY = "assist_satellite.jarvis_iphone"
+SOLAR_MAX_W = 7000
+SURPLUS_ON_W = 1500
+SURPLUS_OFF_W = 500
+REFRESH_SECONDS = 5
