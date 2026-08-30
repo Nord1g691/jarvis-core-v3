df2278085a41766f8b1071c53c1b8c074e78c345
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-08-31 01:16:01 +0200

    fix: use correct custom panel js_url for V13 loader

--- custom_components/jarvis/__init__.py
+++ custom_components/jarvis/__init__.py
@@ -31,7 +31,7 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
         config={
             "_panel_custom": {
                 "name": "jarvis-core-hud",
-                "module_url": PANEL_MODULE,
+                "js_url": PANEL_MODULE,
                 "embed_iframe": False,
                 "trust_external": False,
             }
