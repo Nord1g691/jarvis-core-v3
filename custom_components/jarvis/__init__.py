e7555207389e56730eee48f63d3ec96a094de96d
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-09-02 13:06:36 +0200

    Register read-only JARVIS suggestions API

--- custom_components/jarvis/__init__.py
+++ custom_components/jarvis/__init__.py
@@ -15,6 +15,7 @@ from .context import JarvisContextView
 from .conversation import JarvisConversationView
 from .diagnostics import JarvisDiagnosticsView
 from .memory import JarvisMemoryView
+from .suggestions import JarvisSuggestionsView
 
 PLATFORMS = ["sensor"]
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
@@ -30,6 +31,7 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     hass.http.register_view(JarvisContextView(hass))
     hass.http.register_view(JarvisBriefingView(hass))
     hass.http.register_view(JarvisDiagnosticsView(hass))
+    hass.http.register_view(JarvisSuggestionsView(hass))
     frontend_dir = Path(__file__).parent / "frontend"
     await hass.http.async_register_static_paths(
         [StaticPathConfig(FRONTEND_URL, str(frontend_dir), cache_headers=False)]
